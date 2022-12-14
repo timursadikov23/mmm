f8ac186e6ea3e1907ee6fe44b78f51b0fc004ff1
unknown <timurkakhkharov@gmail.com>
2022-12-15 02:56:28 +0500

    mm

--- assets/js/fancy.js
+++ assets/js/fancy.js
@@ -4,6 +4,8 @@ const titleProduct = document.querySelector('#title-product');
 const titleProd = document.querySelector('#title-product1');
 const cardProduct = document.querySelector('#card-product');
 const cardProduct1 = document.querySelector('#card-product1');
+const showMore = document.querySelector('#page-btn');
+const hidPage = document.querySelector('#hidden-pages');
 
 
 
@@ -25,4 +27,9 @@ cardProduct1.addEventListener('mouseover', () => {
 cardProduct1.addEventListener('mouseout', () => {
   titleProduct.classList.remove('active')
   titleProd.classList.remove('active')
+})
+
+showMore.addEventListener('click', () => {
+  hidPage.classList.remove('active-page')
+  
 })
